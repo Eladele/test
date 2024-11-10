11092c176dd5cfd3cf2a3fc0b1e815930ceed46b
eladel <eladelmahmawden@gmail.com>
2024-11-10 00:22:12 +0000

    1er commit

---
+++ src/main/java/com/example/products/entity/Product.java
@@ -0,0 +1,25 @@
+package com.example.products.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String image;
+    private Double price;
+    private Integer rating;
+
+    // Getters et setters
+}
